6dfc73e876c1a525117bbd6ac2137a1bba342306
Amaze Mkhonto <amaze.mkhonto2@gmail.com>
2024-03-30 05:13:49 +0200

    Create AccountHolder Database

---
+++ Bank.Service.AccountHolder/Migrations/20240330031158_seedDataAccountHolder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bank.Service.AccountHolder.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedDataAccountHolder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AccountHolders",
+                columns: new[] { "Id", "DateOfBirth", "EmailAddress", "FirstName", "IdNumber", "LastName", "MobileNumber", "ResidentialAddress" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1980, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "john.doe@example.com", "John", "8001010000000", "Doe", "0123456789", "123 Main Street, Cape Town" },
+                    { 2, new DateTime(1995, 3, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "jane.smith@example.com", "Jane", "7503150000000", "Smith", "987654321", "456 Pine Avenue, Johannesburg" },
+                    { 3, new DateTime(1960, 7, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "michael.jones@example.com", "Michael", "6007010000000", "Jones", "0009876543", "789 Oak Lane, Durban" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AccountHolders",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AccountHolders",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AccountHolders",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- Bank.Service.AccountHolder/Models/AccountHolder.cs
+++ Bank.Service.AccountHolder/Models/AccountHolder.cs
@@ -2,19 +2,23 @@
 
 namespace Bank.Service.AccountHolder.Models
 {
-    public class AccountHolder
+    public class AccountHolders
     {
       
             [Key]
             public int Id { get; set; }
+            [Required]
             public string FirstName { get; set; }
+            [Required]
             public string LastName { get; set; }
+            [Required]
             public DateTime DateOfBirth { get; set; }
             [Required]
             public string IdNumber { get; set; }
             public string ResidentialAddress { get; set; }
-            public string MobileNumber { get; set; }
             [Required]
+            public string MobileNumber { get; set; }
+            
             public string EmailAddress { get; set; }
 
             // You can add a collection of BankAccounts if relevant to your design
